2674e6121df188dbfb45af726853fa78adfde36c
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-07-06 19:48:48 -0500

    init commit

---
+++ src/main/java/com/codeup/springblog/controllers/Post.java
@@ -0,0 +1,42 @@
+package com.codeup.springblog.controllers;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String body;
+
+    public Post(){}
+
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle(){ return title; }
+
+    public void setTitle(String title) { this.title = title;}
+
+    public String getBody() { return body; }
+
+    public void setBody(String body) {this.body = body; }
+}
